a560d17639dd86651bc72bad3234f1e9519ffc9b
EC2 Default User <ec2-user@ip-172-31-2-254.ec2.internal>
2021-04-04 12:48:19 +0000

    "[fix]"validates"

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,24 +15,24 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     if @book.save
+
       flash[:notice]="You have creatad book successfully."
       redirect_to book_path(@book)
     else
-      @user = current_user
-      @books = Book.all
-      render :index
+      @books=Book.all
+      render "index"
     end
   end
 
   def edit
     @book = Book.find(params[:id])
     if @book.user == current_user
-        render "edit"
+      render :edit
     else
-        redirect_to books_path
+      redirect_to books_path
     end
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
@@ -51,11 +51,11 @@ class BooksController < ApplicationController
       redirect_to books_path
     end
   end
-  
-  
+
+
     private
     def book_params
       params.require(:book).permit(:title, :body)
     end
-    
+
 end
